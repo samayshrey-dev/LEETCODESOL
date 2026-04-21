05a65801736cd31c6bbeec0df838899cdad696bd
Samayshrey patnaik <samayshrey.p@gmail.com>
2026-04-21 11:13:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (34.80%)

---
+++ solutions/0035-search-insert-position/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int searchInsert(int nums[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+        else if(nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return left; // insert position
+}
