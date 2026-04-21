d6bff901b70b6aea3e192bcaa31c1d7bf8c0d127
Samayshrey patnaik <samayshrey.p@gmail.com>
2026-04-21 11:09:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.5 MB (51.92%)

---
+++ solutions/0053-maximum-subarray/solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int maxSubArray(int nums[], int n) {
+    int current_sum = nums[0];
+    int max_sum = nums[0];
+
+    for(int i = 1; i < n; i++) {
+        if (current_sum + nums[i] > nums[i])
+            current_sum = current_sum + nums[i];
+        else
+            current_sum = nums[i];
+
+        if (current_sum > max_sum)
+            max_sum = current_sum;
+    }
+
+    return max_sum;
+}
